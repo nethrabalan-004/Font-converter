f0bddf83c0c21aeef4ba9fa2621bf418a549dc94
nethrabalan-004 <nethrabalan05@gmail.com>
2023-12-05 11:39:40 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,56 @@
+var s=document.getElementById("get");
+var a1=document.getElementById("f1");
+var a2=document.getElementById("f2");
+var a3=document.getElementById("f3");
+var a4=document.getElementById("f4");
+var a5=document.getElementById("f5");
+var a6=document.getElementById("f6");
+var a7=document.getElementById("f7");
+var a8=document.getElementById("f8");
+var a9=document.getElementById("f9");
+var a10=document.getElementById("f10");
+var a11=document.getElementById("f11");
+var a12=document.getElementById("f12");
+var a13=document.getElementById("f13");
+var a14=document.getElementById("f14");
+var a15=document.getElementById("f15");
+var a16=document.getElementById("f16");
+var a17=document.getElementById("f17");
+var a18=document.getElementById("f18");
+var a19=document.getElementById("f19");
+var a20=document.getElementById("f20");
+var a21=document.getElementById("f21");
+var a22=document.getElementById("f22");
+var a23=document.getElementById("f23");
+var a24=document.getElementById("f24");
+var a25=document.getElementById("f25");
+
+function ch()
+{
+    
+  a1.textContent = s.value;
+  a2.textContent = s.value
+  a3.textContent = s.value
+  a4.textContent =s.value
+  a5.textContent = s.value
+  a6.textContent = s.value
+  a7.textContent = s.value
+  a8.textContent = s.value
+  a9.textContent= s.value
+  a10.textContent= s.value
+  a11.textContent= s.value
+  a12.textContent= s.value
+  a13.textContent= s.value
+  a14.textContent= s.value
+  a15.textContent= s.value
+  a16.textContent= s.value
+  a17.textContent= s.value
+  a18.textContent= s.value
+  a19.textContent= s.value
+  a20.textContent= s.value
+  a21.textContent= s.value
+  a22.textContent= s.value
+  a23.textContent= s.value
+  a24.textContent= s.value
+  a25.textContent= s.value
+}
